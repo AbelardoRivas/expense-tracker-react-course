789ec2bde7f99556f39e21556d8cb8f04ff03590
AbelardoRivas <abelardo.rivas1@gmail.com>
2023-09-21 20:58:08 -0400

    lesson 64

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import componentsImage from "./assets/images/components.png";
 import stateImage from "./assets/images/state.png";
 import eventsImage from "./assets/images/events.png";
 import Header from "./components/Header/Header";
+import Concept from "./components/Concept/Concept";
 
 const concepts = [
   {
@@ -29,21 +30,21 @@ function App() {
     <div>
       <Header />
       <ul id="concepts">
-        <li className="concept">
-          <img src={concepts[0].image} alt={concepts[0].title} />
-          <h2>{concepts[0].title}</h2>
-          <p>{concepts[0].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[1].image} alt={concepts[1].title} />
-          <h2>{concepts[1].title}</h2>
-          <p>{concepts[1].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[2].image} alt={concepts[2].title} />
-          <h2>{concepts[2].title}</h2>
-          <p>{concepts[2].description}</p>
-        </li>
+        <Concept
+          title={concepts[0].title}
+          image={concepts[0].image}
+          description={concepts[0].description}
+        />
+        <Concept
+          title={concepts[1].title}
+          image={concepts[1].image}
+          description={concepts[1].description}
+        />
+        <Concept
+          title={concepts[2].title}
+          image={concepts[2].image}
+          description={concepts[2].description}
+        />
       </ul>
     </div>
   );
